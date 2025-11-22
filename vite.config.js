e51ee4a8a84a59d3782ee25f58e7cb8f855e9a27
Satyam Kumar <satyam@lumina.app>
2025-11-22 20:09:49 +0530

    Fix Babel plugin error: Remove unnecessary config

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       'react-native': 'react-native-web',
